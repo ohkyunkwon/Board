c8fef433c6acdb71c1ee41eaef93f73248813e41
kwon-ohkyun <kok9021@gmail.com>
2023-10-14 00:20:09 +0900

    springboot 입문 mysql 연결

--- src/main/java/hello/springhello17/service/SpringConfig.java
+++ src/main/java/hello/springhello17/service/SpringConfig.java
@@ -1,22 +1,40 @@
 package hello.springhello17.service;
+import hello.springhello17.repository.JdbcMemberRepository;
 import hello.springhello17.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
-    private MemberRepository memberRepository;
-    @Autowired
-    public SpringConfig(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
-
     @Bean
-    public MemberService memberService(){
-
-        return new MemberService(memberRepository);
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
     }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
+    }
+}
+//    private MemberRepository memberRepository;
+//    @Autowired
+//    public SpringConfig(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Bean
+//    public MemberService memberService(){
+//
+//        return new MemberService(memberRepository);
+//    }
+
+
+
     //    @Bean
     //    public TimeTraceAop timeTraceAop(){
     //        return new TimeTraceAop();
@@ -29,4 +47,3 @@ public class SpringConfig {
     //        return new JdbcTemplateMemberRepository(dataSource);
     //        return new JpaMemberRepository(em);
     //   }
-}
